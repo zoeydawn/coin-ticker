d7bb5c3159dcb9730f11357842f7955afe57c88e
Donovan <donovan6925@gmail.com>
2017-01-29 21:20:20 -0500

    added 5 exchanges

--- test/test.js
+++ test/test.js
@@ -4,9 +4,66 @@ const { expect } = chai;
 const coinTicker = require('../index.js');
 
 describe('coinTicker', () => {
-  // console.log('coinTicker:', coinTicker('bitfinex'));
-  it('Should return object of Bitfinex data', () => {
-    expect(coinTicker('bitfinex')).to.have.property('last');
+
+  it('Should return an object of Bitfinex data', () => {
+    return coinTicker('bitfinex').then((data) => {
+      expect(data).to.be.an('object');
+      expect(data).to.have.all.keys('last', 'ask', 'bid', 'low', 'high', 'vol', 'timestamp', 'exchange', 'pair');
+      expect(data.last).to.be.a('string');
+      expect(data.low).to.be.a('string');
+      expect(data.high).to.be.a('string');
+      expect(data.vol).to.be.a('string');
+      expect(data.exchange).to.equal('Bitfinex');
+    })
+  })
+
+  it('Should return an object of BTC-e data', () => {
+    return coinTicker('btce').then((data) => {
+      expect(data).to.be.an('object');
+      expect(data).to.have.all.keys('last', 'ask', 'bid', 'low', 'high', 'vol', 'timestamp', 'exchange', 'pair');
+      expect(data.last).to.be.a('string');
+      expect(data.low).to.be.a('string');
+      expect(data.high).to.be.a('string');
+      expect(data.vol).to.be.a('string');
+      expect(data.exchange).to.equal('BTC-e');
+    })
+  })
+
+  it('Should return an object of Bitstamp data', () => {
+    return coinTicker('bitstamp').then((data) => {
+      expect(data).to.be.an('object');
+      expect(data).to.have.all.keys('last', 'ask', 'bid', 'low', 'high', 'vol', 'timestamp', 'exchange', 'pair');
+      expect(data.last).to.be.a('string');
+      expect(data.low).to.be.a('string');
+      expect(data.high).to.be.a('string');
+      expect(data.vol).to.be.a('string');
+      expect(data.exchange).to.equal('Bitstamp');
+    })
+  })
+
+  it('Should return an object of Kraken data', () => {
+    return coinTicker('kraken').then((data) => {
+      expect(data).to.be.an('object');
+      expect(data).to.have.all.keys('last', 'ask', 'bid', 'low', 'high', 'vol', 'timestamp', 'exchange', 'pair');
+      expect(data.last).to.be.a('string');
+      expect(data.low).to.be.a('string');
+      expect(data.high).to.be.a('string');
+      expect(data.vol).to.be.a('string');
+      expect(data.exchange).to.equal('Kraken');
+    })
+  })
+
+  it('Should return an object of Okcoin data', () => {
+    return coinTicker('okcoin').then((data) => {
+      // console.log('data:', data);
+      expect(data).to.be.an('object');
+      expect(data).to.have.all.keys('last', 'ask', 'bid', 'low', 'high', 'vol', 'timestamp', 'exchange', 'pair');
+      expect(data.last).to.be.a('string');
+      expect(data.low).to.be.a('string');
+      expect(data.high).to.be.a('string');
+      expect(data.vol).to.be.a('string');
+      expect(data.exchange).to.equal('Okcoin');
+    })
   })
 
   // it('Should throw error if given invalid arguments', () => {
