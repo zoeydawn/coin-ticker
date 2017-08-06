fb6464da7b9313bab8085a7db6c41541fc18b3a7
Donovan <donovan6925@gmail.com>
2017-08-06 15:17:44 -0400

    formated kraken asset symbols

--- pairs/exchanges/kraken.js
+++ pairs/exchanges/kraken.js
@@ -9,6 +9,16 @@ const format = (asset) => {
   return assetAlts[asset] || asset;
 }
 
+const trimXAndZ = (asset) => {
+  let trimmedAsset = asset;
+  if (trimmedAsset.length > 3) {
+    trimmedAsset = trimmedAsset[0] === 'X' ? trimmedAsset.substr(1) : trimmedAsset;
+    const lastChar = trimmedAsset[trimmedAsset.length - 1];
+    trimmedAsset = lastChar === 'Z' || lastChar === 'X' ? trimmedAsset.substr(0, trimmedAsset.length-1) : trimmedAsset;
+  }
+  return format(trimmedAsset);
+}
+
 module.exports = () => {
   return axios.get('https://api.kraken.com/0/public/AssetPairs')
     .then((res) => {
@@ -19,8 +29,10 @@ module.exports = () => {
           ? pair.substr(pair.length - 5)
           : pair.substr(pair.length - 3);
 
-        return format(first) + '_' + format(second);
-      });
+        return trimXAndZ(first) + '_' + format(second);
+      }).filter((pair) => (
+        !pair.includes('.')
+      ));
     })
     .catch((err) => {
       console.error('Error fetching kraken pairs:', err);
